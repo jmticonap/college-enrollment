71d519a5d381a85ad6883940cf4965581b1a3e7a
juancho <juan.ticona@tektonlabs.com>
2023-04-14 14:24:09 -0500

    FIX env. vars.

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -3,13 +3,13 @@ import { env } from 'process';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: env.DB_HOST || 'localhost',
-  port: parseInt(env.DB_PORT, 10) || 5432,
-  username: env.DB_USERNAME || 'postgres',
-  password: env.DB_PASSWORD || 'password',
-  database: env.DB_NAME,
+  host: env.DB_HOST ?? 'localhost',
+  port: parseInt(env.DB_PORT, 10) ?? 5432,
+  username: env.DB_USERNAME ?? 'postgres',
+  password: env.DB_PASSWORD ?? 'password',
+  database: env.DB_NAME ?? 'college-enrolment-db',
   entities: ['dist/**/*.entity.js'],
-  poolSize: parseInt(env.DB_POOL_SIZE, 10) || 10,
+  poolSize: parseInt(env.DB_POOL_SIZE, 10) ?? 10,
   synchronize: !['dev', 'test'].includes(env.NODE_ENV),
 };
 
