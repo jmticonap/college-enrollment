f6af9eb94560a333db158dbffd21a7fca5b3e2e7
juancho <juan.ticona@tektonlabs.com>
2023-04-15 10:49:18 -0500

    FIX port and host, values come from .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'node:process';
 
 async function bootstrap() {
+  const { APP_PORT, APP_HOST } = env;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APP_PORT ?? 3000, APP_HOST ?? 'localhost');
 }
 bootstrap();
