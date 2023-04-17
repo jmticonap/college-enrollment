3c397c34758f59032329db6b161f90e56cc2bb21
juancho <juan.ticona@tektonlabs.com>
2023-04-17 15:33:35 -0500

    FIX import statements order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CreateUpdateInterceptor } from './logging/createUpdate.interceptor';
 import { ErrorInterceptor } from './logging/error.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ProfessorModule } from './professor/professor.module';
 import { StudentModule } from './student/student.module';
 import { EnrollmentModule } from './enrollment/enrollment.module';
 import { dataSourceOptions } from './db/dataSource';
 import { EnrollCourseModule } from './enroll-course/enroll-course.module';
 import { CourseModule } from './course/course.module';
-import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
 
 @Module({
