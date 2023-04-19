1cdd7174bc6a6492d4afe4b681e238953c9a9d76
juancho <juan.ticona@tektonlabs.com>
2023-04-19 11:38:12 -0500

    ADD various

---
+++ scripts/professor.create.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:3000/professor \
+  -H 'Content-Type: application/json' \
+  -d '{"firstname": "Kobe","lastname": "Bryamt","dni": "41927101","phone": "+53 942 411 785","address": "Av. Mariscal Castilla 506"}'
