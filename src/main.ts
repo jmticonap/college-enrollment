6d3f7fc1d9053edad0bc9a7a28ec082c8d99c474
juancho <juan.ticona@tektonlabs.com>
2023-04-24 15:03:35 -0500

    SET cors as enable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'node:process';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const { APP_PORT, APP_HOST } = env;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(APP_PORT ?? 3000, APP_HOST ?? 'localhost');
 }
+
+// AppService.clusterize(bootstrap);
 bootstrap();
