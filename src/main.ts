e627fe7053dc0b17ae15e9ddb069de49b38caa91
juancho <juan.ticona@tektonlabs.com>
2023-04-25 10:18:33 -0500

    ADD helmet & cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'node:process';
+import helmet from 'helmet';
 import { AppService } from './app.service';
 
 async function bootstrap() {
   const { APP_PORT, APP_HOST } = env;
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   await app.listen(APP_PORT ?? 3000, APP_HOST ?? 'localhost');
 }
 
