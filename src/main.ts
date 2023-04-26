bca566c27be81224ee078efed0d9848e6010ea39
juancho <juan.ticona@tektonlabs.com>
2023-04-26 17:44:34 -0500

    Update main.ts
    FIX env PORT variable

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { env } from 'node:process';
 import helmet from 'helmet';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const { APP_PORT } = env;
+  const { PORT } = env;
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
-  await app.listen(APP_PORT ?? 3000);
+  await app.listen(PORT ?? 3000);
 }
 
 // AppService.clusterize(bootstrap);
